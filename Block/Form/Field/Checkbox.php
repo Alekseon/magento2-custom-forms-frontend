892083d374186b4a7316772de7fe50286d0a89ff
Marcin Frymark <m.frymark@alekseon.com>
2023-06-02 20:17:57 +0200

    frontend inputs moved to Alekseon_CustomFormsFrontend

---
+++ Block/Form/Field/Checkbox.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright © Alekseon sp. z o.o.
+ * http://www.alekseon.com/
+ */
+declare(strict_types=1);
+
+namespace Alekseon\CustomFormsFrontend\Block\Form\Field;
+
+use Alekseon\AlekseonEav\Model\Attribute\InputType\AbstractInputType;
+use Alekseon\AlekseonEav\Model\Attribute\Source\Boolean;
+
+/**
+ * Class Checkbox
+ * @package Alekseon\CustomFormsFrontend\Block\Form\Field
+ */
+class Checkbox extends \Alekseon\CustomFormsFrontend\Block\Form\Field\Select
+{
+    protected $_template = "Alekseon_CustomFormsFrontend::form/field/checkbox.phtml";
+
+    /**
+     * @return bool
+     */
+    public function displayLabel()
+    {
+        /** @var AbstractInputType $field */
+        $field = $this->getField();
+
+        if ($field->getFrontendInput() == 'boolean') {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @return array
+     */
+    public function getOptions()
+    {
+        $options = parent::getOptions();
+
+        /** @var AbstractInputType $field */
+        $field = $this->getField();
+
+        if ($field->getFrontendInput() == 'boolean') {
+            unset($options[Boolean::VALUE_NO]);
+            $options[Boolean::VALUE_YES] = $this->getLabel();
+        }
+
+        return $options;
+    }
+}
---
+++ Block/Form/Field/Multiselect.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright © Alekseon sp. z o.o.
+ * http://www.alekseon.com/
+ */
+declare(strict_types=1);
+
+namespace Alekseon\CustomFormsFrontend\Block\Form\Field;
+
+/**
+ * Class Multiselect
+ * @package Alekseon\CustomFormsFrontend\Block\Form\Field
+ */
+class Multiselect extends \Alekseon\CustomFormsFrontend\Block\Form\Field\Select
+{
+    protected $_template = "Alekseon_CustomFormsFrontend::form/field/multiselect.phtml";
+}
