729110d9b83438a3a87fcab80664eefe4641aa57
Marcin Frymark <m.frymark@alekseon.com>
2024-11-22 16:44:28 +0100

    100.1.4

--- view/frontend/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    config: {
-        mixins: {
-            'mage/validation': {
-                'Alekseon_CustomFormsFrontend/js/validation-mixin': true
-            }
-        }
-    }
-}
